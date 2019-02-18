7f66e7382770ffcd92c3931c2d7b427e5227bc9b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-18 16:11:20 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(title: params[:post][:title], description: params[:description])
+    @article.update(title: params[:post][:title], description: params[:post][:description])
     redirect_to article_path(@article)
   end
 
